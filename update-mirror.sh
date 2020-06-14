6c7dcb8a75626ee67fe5f7a72d0a17d0c5bcacb1
mi2428 <mi2428782020@gmail.com>
2020-06-15 00:07:00 +0900

    Trivial fix
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- update-mirror.sh
+++ update-mirror.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $(dirname $0)
-/usr/bin/docker-compose up -d pacman apt-ubuntu apt-debian apt-raspberrypi
+/usr/bin/docker-compose up -d pacman-arch apt-ubuntu apt-debian apt-raspberrypi
