24e07854bd831aaefb5c630a4b137d8830be9c57
mi2428 <mi2428782020@gmail.com>
2020-06-18 14:21:40 +0900

    Fix bug
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- portage/gentoo/mirror.sh
+++ portage/gentoo/mirror.sh
@@ -4,6 +4,7 @@ MIRROR_U_SRC='rsync://rsync.jp.gentoo.org/gentoo-portage/'
 MIRROR_I_SRC='rsync://ftp.iij.ad.jp/pub/linux/gentoo/'
 MIRROR_U_DEST='/var/spool/portage-mirror/gentoo-portage'
 MIRROR_I_DEST='/var/spool/portage-mirror/gentoo'
+MIRROR_I_REPO="distfiles experimental releases snapshots"
 
 RSYNC_OPTS="-rtlHv --delete-after --delay-updates --copy-links --safe-links --max-delete=1000 --delete-excluded --exclude=.*"
 RSYNC_EXCLUDE_OPT=""
@@ -16,8 +17,13 @@ fi
 
 echo "Step 1 of 2: Syncing rsync mirror ..."
 rsync $RSYNC_OPTS $RSYNC_EXCLUDE_OPT $MIRROR_U_SRC $MIRROR_U_DEST
+echo
 
 echo "Step 2 of 2: Syncing source mirror ..."
-rsync $RSYNC_OPTS $RSYNC_EXCLUDE_OPT $MIRROR_S_SRC $MIRROR_S_DEST
+for REPO in $MIRROR_I_REPO; do
+    echo "Syncing $REPO ..."
+    rsync $RSYNC_OPTS $RSYNC_EXCLUDE_OPT $MIRROR_I_SRC/$REPO $MIRROR_I_DEST
+    echo 
+done
 
 exit 0
